0d298af3e7ad7bf787bb5097119a6e6a63454dcb
Jae Min Kim <presso.kim@kurlycorp.com>
2025-07-27 17:19:42 +0900

    feat: react-hook-form 및 zod 통합하여 FormProvider 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,10 @@ import type { AppProps } from 'next/app';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from '@/lib/queryClient';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { FormProvider, useForm } from 'react-hook-form';
+import { BookReviewFormSchema, type BookReviewForm } from '@/types/BookReviewForm';
+import { ReadingStatus } from '@/types/BookInfo';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 const theme = createTheme({
   palette: {
@@ -11,12 +15,31 @@ const theme = createTheme({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
+  const methods = useForm<BookReviewForm>({
+    resolver: zodResolver(BookReviewFormSchema),
+    defaultValues: {
+      title: '',
+      author: '',
+      totalPages: 0,
+      status: ReadingStatus.WISH_TO_READ,
+      startDate: new Date(),
+      endDate: new Date(),
+      rating: 0,
+      comment: '',
+      quotes: [],
+      recommend: false,
+      visibility: true,
+    },
+  });
+
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
-        <main className="app">
-          <Component {...pageProps} />
-        </main>
+        <FormProvider {...methods}>
+          <main className="app">
+            <Component {...pageProps} />
+          </main>
+        </FormProvider>
       </ThemeProvider>
     </QueryClientProvider>
   );
