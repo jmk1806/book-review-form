ec38af03ccf681c0d37a2a5889536b681f4f269f
Jae Min Kim <presso.kim@kurlycorp.com>
2025-07-27 15:37:03 +0900

    feat: 독후감 작성 페이지의 레이아웃 및 단계별 컴포넌트 구현

--- src/screens/ReviewPage.tsx
+++ src/screens/ReviewPage.tsx
@@ -1,3 +1,119 @@
+import { BookInfo } from '@/components/BookInfo';
+import { STEP_LABELS, STEPS } from '@/constants/step';
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Container,
+  Grid,
+  Step,
+  StepLabel,
+  Stepper,
+  Typography,
+} from '@mui/material';
+import { Fragment, useState } from 'react';
+import ChevronLeftRoundedIcon from '@mui/icons-material/ChevronLeftRounded';
+import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded';
+import { Rating } from '@/components/Rating';
+import { Quotes } from '@/components/Quotes';
+import { Recommend } from '@mui/icons-material';
+import { Visibility } from '@/components/Visibility';
+
 export default function ReviewPage() {
-  return <div>ReviewPage</div>;
+  const [activeStep, setActiveStep] = useState(0);
+  const handleNext = () => {
+    setActiveStep(activeStep + 1);
+  };
+  const handleBack = () => {
+    setActiveStep(activeStep - 1);
+  };
+
+  const getStepContent = (step: number) => {
+    switch (step) {
+      case 0:
+        return <BookInfo />;
+      case 1:
+        return <Rating />;
+      case 2:
+        return <Quotes />;
+      case 3:
+        return <Recommend />;
+      case 4:
+        return <Visibility />;
+      default:
+        throw new Error('Invalid step');
+    }
+  };
+
+  return (
+    <Container maxWidth="xl">
+      <Grid container sx={{ width: '100%', height: '100%', p: 4 }}>
+        <Grid sx={{ display: 'flex', flexDirection: 'column', gap: 2, width: '100%' }}>
+          <Box
+            sx={{ display: 'flex', alignItems: 'center', width: '100%', justifyContent: 'center' }}
+          >
+            <Stepper activeStep={activeStep}>
+              {STEPS.map((step) => (
+                <Step key={step} sx={{ ':first-child': { pl: 0 }, ':last-child': { pr: 0 } }}>
+                  <StepLabel>{STEP_LABELS[step]}</StepLabel>
+                </Step>
+              ))}
+            </Stepper>
+          </Box>
+          <Card sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+            <CardContent
+              sx={{
+                display: 'flex',
+                width: '100%',
+                flexDirection: 'column',
+                gap: 2,
+              }}
+            >
+              <Typography variant="h6">{STEP_LABELS[STEPS[activeStep]]}</Typography>
+              {getStepContent(activeStep)}
+            </CardContent>
+          </Card>
+        </Grid>
+        <Fragment>
+          <Box
+            sx={[
+              {
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'end',
+                flexGrow: 1,
+                gap: 2,
+                mt: 4,
+              },
+              activeStep !== 0
+                ? { justifyContent: 'space-between' }
+                : { justifyContent: 'flex-end' },
+            ]}
+          >
+            {activeStep !== 0 && (
+              <Button
+                startIcon={<ChevronLeftRoundedIcon />}
+                onClick={handleBack}
+                variant="outlined"
+                sx={{ display: 'flex' }}
+              >
+                이전
+              </Button>
+            )}
+            {activeStep !== STEPS.length - 1 && (
+              <Button
+                variant="contained"
+                endIcon={<ChevronRightRoundedIcon />}
+                onClick={handleNext}
+                sx={{ width: 'fit-content' }}
+              >
+                {activeStep === STEPS.length - 1 ? '완료' : '다음'}
+              </Button>
+            )}
+          </Box>
+        </Fragment>
+      </Grid>
+    </Container>
+  );
 }
