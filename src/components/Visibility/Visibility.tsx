6eb1c761cc283ed261f9b7c48a05ebcd4fd5a3a5
Jae Min Kim <presso.kim@kurlycorp.com>
2025-07-27 16:21:42 +0900

    feat: 공개 여부 컴포넌트 추가

--- src/components/Visibility/Visibility.tsx
+++ src/components/Visibility/Visibility.tsx
@@ -1,3 +1,24 @@
+import { FormLabel, RadioGroup, FormControlLabel, Radio, TextField } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
+
+const FormGrid = styled(Grid)(() => ({
+  display: 'flex',
+  flexDirection: 'column',
+}));
+
 export function Visibility() {
-  return <div>Visibility</div>;
+  return (
+    <Grid container spacing={3}>
+      <FormGrid size={12}>
+        <FormLabel htmlFor="book-visibility" required>
+          도서 공개 여부
+        </FormLabel>
+        <RadioGroup sx={{ display: 'flex', flexDirection: 'row', gap: 2 }}>
+          <FormControlLabel value="true" control={<Radio />} label="공개" />
+          <FormControlLabel value="false" control={<Radio />} label="비공개" />
+        </RadioGroup>
+      </FormGrid>
+    </Grid>
+  );
 }
