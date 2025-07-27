2279c6dddde0b9390e3f01f8f1920e56f52a6263
Jae Min Kim <presso.kim@kurlycorp.com>
2025-07-27 12:56:29 +0900

    feat: 독후감 작성 페이지 및 컴포넌트 추가

---
+++ src/pages/review/index.tsx
@@ -0,0 +1,34 @@
+import Head from 'next/head';
+import { Geist, Geist_Mono } from 'next/font/google';
+import styles from '@/styles/Home.module.css';
+import ReviewPage from '@/screens/ReviewPage';
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export default function Page() {
+  return (
+    <>
+      <Head>
+        <title>독후감 작성하기</title>
+        <meta name="description" content="독후감 작성하기" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}>
+        <main className={styles.main}>
+          <ReviewPage />
+        </main>
+        <footer className={styles.footer}>
+          Made by <a href="https://github.com/jmk1806">Jae Min Kim</a>
+        </footer>
+      </div>
+    </>
+  );
+}
