42b22ab911ebbdd88c118d25e1e254c4d0aee0d2
Jae Min Kim <kjm91806@gmail.com>
2025-08-18 23:08:10 +0900

    fix: FormProvider app레벨에서 페이지 레벨로 이동

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,7 @@ import type { AppProps } from 'next/app';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from '@/lib/queryClient';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { FormProvider, useForm } from 'react-hook-form';
-import { BookReviewFormSchema, type BookReviewForm } from '@/types/BookReviewForm';
-import { ReadingStatus } from '@/types/BookInfo';
-import { zodResolver } from '@hookform/resolvers/zod';
+
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
@@ -17,37 +14,13 @@ const theme = createTheme({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
-  const methods = useForm<BookReviewForm>({
-    resolver: zodResolver(BookReviewFormSchema),
-    mode: 'onSubmit',
-    defaultValues: {
-      title: '',
-      author: '',
-      totalPages: 0,
-      status: ReadingStatus.WISH_TO_READ,
-      rating: 0,
-      comment: '',
-      quotes: [
-        {
-          page: 0,
-          text: '',
-        },
-      ],
-      recommend: false,
-      visibility: true,
-      publishDate: new Date(),
-    },
-  });
-
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <FormProvider {...methods}>
-            <main className="app">
-              <Component {...pageProps} />
-            </main>
-          </FormProvider>
+          <main className="app">
+            <Component {...pageProps} />
+          </main>
         </LocalizationProvider>
       </ThemeProvider>
     </QueryClientProvider>
--- src/pages/review/index.tsx
+++ src/pages/review/index.tsx
@@ -1,7 +1,32 @@
 import Head from 'next/head';
 import ReviewPage from '@/screens/ReviewPage';
+import { BookReviewFormSchema, type BookReviewForm } from '@/types/BookReviewForm';
+import { ReadingStatus } from '@/types/BookInfo';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { FormProvider, useForm } from 'react-hook-form';
 
 export default function Page() {
+  const methods = useForm<BookReviewForm>({
+    resolver: zodResolver(BookReviewFormSchema),
+    mode: 'onSubmit',
+    defaultValues: {
+      title: '',
+      author: '',
+      totalPages: 0,
+      status: ReadingStatus.WISH_TO_READ,
+      rating: 0,
+      comment: '',
+      quotes: [
+        {
+          page: 0,
+          text: '',
+        },
+      ],
+      recommend: false,
+      visibility: true,
+      publishDate: new Date(),
+    },
+  });
   return (
     <>
       <Head>
@@ -10,7 +35,9 @@ export default function Page() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <ReviewPage />
+      <FormProvider {...methods}>
+        <ReviewPage />
+      </FormProvider>
     </>
   );
 }
