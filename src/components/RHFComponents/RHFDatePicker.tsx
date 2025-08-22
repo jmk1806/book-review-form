94d119859ee32d4061904aa177caf23a9a522ec1
Jae Min Kim <presso.kim@kurlycorp.com>
2025-08-22 18:06:52 +0900

    feat: RHFDatePicker 컴포넌트 추가

---
+++ src/components/RHFComponents/RHFDatePicker.tsx
@@ -0,0 +1,47 @@
+import { RHFDatePickerProps } from '@/types/RHFComponents';
+import type { BookReviewForm } from '@/types/BookReviewForm';
+import { useFormContext, Controller } from 'react-hook-form';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import dayjs from 'dayjs';
+
+export function RHFDatePicker({
+  id,
+  name,
+  onChange,
+  size = 'small',
+  disabled = false,
+}: RHFDatePickerProps) {
+  const {
+    control,
+    setValue,
+    formState: { errors },
+  } = useFormContext<BookReviewForm>();
+
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field }) => (
+        <DatePicker
+          {...field}
+          value={field.value && field.value instanceof Date ? dayjs(field.value) : null}
+          onChange={(newValue) => {
+            const dateValue = newValue ? newValue.toDate() : null;
+            field.onChange(dateValue);
+            if (onChange) {
+              onChange(dateValue, setValue);
+            }
+          }}
+          slotProps={{
+            textField: {
+              size,
+              error: Boolean(errors[name]),
+              helperText: errors[name]?.message,
+            },
+          }}
+          disabled={disabled}
+        />
+      )}
+    />
+  );
+}
--- src/components/RHFComponents/index.ts
+++ src/components/RHFComponents/index.ts
@@ -1,3 +1,4 @@
 export { RHFTextField } from './RHFTextField';
 export { RHFCommaSeparatedTextField } from './RHFCommaSeparatedTextField';
 export { RHFSelect } from './RHFSelect';
+export { RHFDatePicker } from './RHFDatePicker';
